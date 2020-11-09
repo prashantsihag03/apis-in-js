3aaaab1f981e4cece2d81106ab69a3c8d5a32dae
prashantsihag03 <prashantsihag0309@gmail.com>
2020-11-09 19:13:07 +1030

    restructured previous three repositories into one

---
+++ tempChart/tempChart.js
@@ -0,0 +1,47 @@
+async function getdata() {
+    const xs = [],
+        ys = [];
+    const response = await fetch('ZonAnn.Ts+dSST.csv');
+    const data = await response.text();
+    const table = data.split('\n').slice(1);
+    table.forEach(row => {
+        const columns = row.split(',');
+        const year = columns[0];
+        xs.push(year);
+        const temp = columns[1];
+        ys.push(parseFloat(temp) + 14);
+        console.log(year, temp);
+    })
+    return { xs, ys };
+};
+
+async function chartIt() {
+    const data = await getdata();
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: data.xs,
+            datasets: [{
+                label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in C°',
+                data: data.ys,
+                fill: false,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: ['rgba(255, 159, 64, 1)'],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        callback: function(value, index, values) {
+                            return value + '°';
+                        }
+                    }
+                }]
+            }
+        }
+    })
+}
+document.getElementsByTagName('body')[0].addEventListener('load', chartIt());
---
+++ whereISSat/whereISSat.js
@@ -0,0 +1,37 @@
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+
+var mymap = L.map('issMap').setView([0, 0], 2);
+
+var myIcon = L.icon({
+    iconUrl: 'iss200.png',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16]
+});
+
+const marker = L.marker([0, 0], {icon: myIcon}).addTo(mymap);
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`;  
+const tiles = L.tileLayer( tileUrl, { attribution });
+tiles.addTo(mymap);
+
+// var firsttime = true;
+
+async function getISS() {
+    const response = await fetch(api_url);
+    const data = await response.json();
+    const latitude = data.latitude;
+    const longitude = data.longitude;
+
+    marker.setLatLng([latitude, longitude]);
+    mymap.setView([latitude, longitude], 2);
+
+    // below code for not moving map every time to center the marker
+    // if (firsttime) {
+    //     mymap.setView([latitude, longitude], 2);
+    //     firsttime = false;
+    // }
+    document.getElementById('lat').textContent = latitude.toFixed(2);
+    document.getElementById('lon').textContent = longitude.toFixed(2);
+    setTimeout(getISS, 1000);
+};
